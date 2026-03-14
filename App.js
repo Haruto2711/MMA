23796a6ed466405a392429426f71fdadfe21295c
Haruto2711 <lolaugus8@gmail.com>
2026-03-14 23:51:51 +0700

    Update App.js

--- App.js
+++ App.js
@@ -1,17 +1,13 @@
 import React from "react";
-<<<<<<< HEAD
 import { SafeAreaView, StyleSheet } from "react-native";
 import { AuthProvider } from "./context/AuthContext";
 import AppNavigator from "./navigation/AppNavigator";
-=======
 import { SafeAreaView,  StyleSheet } from "react-native";
 import HomeScreen from "./screens/HomeScreen";
->>>>>>> 21bea9426ad96123d3b715e297a3a65925540d40
 
 export default function App() {
 
   return (
-<<<<<<< HEAD
 
     <AuthProvider>
 
@@ -23,11 +19,9 @@ export default function App() {
 
     </AuthProvider>
 
-=======
      <SafeAreaView style={styles.container}>
       <HomeScreen />
     </SafeAreaView>
->>>>>>> 21bea9426ad96123d3b715e297a3a65925540d40
   );
 
 }
